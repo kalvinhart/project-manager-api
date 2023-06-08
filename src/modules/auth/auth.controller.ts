70859b7f3c50829d571259c2184326d304bac241
Kalvin <kalvinhart@hotmail.co.uk>
2023-06-08 23:14:31 +0100

    added auth guard

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "src/guards/auth/auth.guard";
 import { UserDto } from "../user/dto/user.dto";
 import { AuthService } from "./auth.service";
 import { CreatePaidUserDto } from "./dto/create-paid-user.dto";
@@ -20,6 +21,7 @@ export class AuthController {
     return await this.authService.createPaidUser(createPaidUserDto);
   }
 
+  @UseGuards(AuthGuard)
   @Post("create")
   async createUser(@Body() createUserDto: CreateUserDto): Promise<UserDto> {
     return await this.authService.createUser(createUserDto);
