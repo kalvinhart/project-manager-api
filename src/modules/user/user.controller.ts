5512d918bed4288cd4b125d781053d87bf217b77
Kalvin <kalvinhart@hotmail.co.uk>
2023-06-23 23:18:19 +0100

    changed route to use id from user token

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,16 +1,18 @@
-import { Controller, Get, Param, UseGuards } from "@nestjs/common";
+import { Controller, Get, Req, UseGuards } from "@nestjs/common";
 import { AuthGuard } from "src/guards/auth/auth.guard";
 import { UserDto } from "./dto/user.dto";
 import { UserService } from "./user.service";
+import { Request } from "express";
 
 @UseGuards(AuthGuard)
 @Controller("user")
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @Get(":id")
-  async getUserById(@Param("id") id: string): Promise<UserDto> {
-    const user = await this.userService.getUserById(id);
-    return new UserDto(user);
+  @Get("/")
+  async getUserDetails(@Req() request: Request): Promise<UserDto> {
+    const { user } = request;
+    const userDetails = await this.userService.getUserById(user._id);
+    return new UserDto(userDetails);
   }
 }
